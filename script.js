96fd1e189637ce671deac1a0c41782e67674e100
sujj <mm4324@gmail.com>
2017-03-12 15:31:51 +0800

    upgrade to lib v1.4

--- script.js
+++ script.js
@@ -5,32 +5,31 @@
  * that can be found in the LICENSE file.
  * @version 1.11
  ***********************************************/
+'use strict';
 
 // get frame/ iframe content by AJAX
 function getFrameContent(url){
-	'use strict';
 	var xml = new XMLHttpRequest(),
-		res = '',
 		text = xml.onreadystatechange = function(){
-		if(xml.readyState == 4){
-			res = xml.responseText;
-			if(res !='') {
-				var pure = getPlainTextHTML(res),
-					regex = /(裁判字號：[\s\n]*([^\n]+))/gim,
-					file = (regex.test(pure))?pure.match(regex)[0].replace(/\s+|裁判字號：/gi,''):'file';
-				executeDL(file, getReJDocString(pure));
+			if(xml.readyState == 4){
+				if(xml.responseText !='') compileJDocText(xml.responseText);
 			}
-		}
-	};
+		};
 
 	xml.open('GET', url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime(), true);
 	xml.send();
+	return text;
+}
 
+function compileJDocText(input){
+	var pure = getPlainTextHTML(input),
+		regex = /(裁判字號：[\s\n]*([^\n]+))/gim,
+		file = regex.test(pure) ? pure.match(regex)[0].replace(/\s+|裁判字號：/gi,''):'file';
+	executeDL(file, getReJDocString(pure));
 }
 
 // create a context menu
 chrome.runtime.onInstalled.addListener(function(){
-	'use strict';
 	chrome.contextMenus.create({
 		title: '司法文書重排版',
 		id: 'relayout-judicial-doc',
@@ -45,9 +44,8 @@ chrome.runtime.onInstalled.addListener(function(){
 
 // trigger function
 chrome.contextMenus.onClicked.addListener(function(info, tab){
-	'use strict';
 	if(info.menuItemId === 'no-blank-copy'){
-		// because the limit of chrome selection text, it returns string with out wrap and line breaks. 
+		// because the limit of chrome selection text, it returns string with out wrap and line breaks.
 		// Only thing we can do is to replace all the blanks.
 		var _term = info.selectionText.replace(/\s+/gim,'');
 		executeCopy(_term);
